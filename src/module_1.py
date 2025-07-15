511ac7b0153c570ecb037701fdb1f30ecdd60d1a
Subrahmanya Gonella <164250924+satishgonella2024@users.noreply.github.com>
2025-07-15 22:40:45 +0100

    Initial commit: QLCapsule
    Generated by Quantum Layer Platform

---
+++ src/module_1.py
@@ -0,0 +1,5 @@
+Here's the implementation of the intentionally incorrect 'add' method in the 'Calculator' class:
+
+class Calculator:
+    def add(self, a, b):
+        return a - b  # This is an intentionally incorrect implementation that will fail a specific test case
